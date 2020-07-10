212c22f2e461afede7637e18288d23b58a15002a
Till Lorentzen <till@autoretouch.com>
2020-07-10 10:35:06 +0200

    Initial Spring Initializer Result

---
+++ src/test/java/com/autoretouch/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoretouch.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
